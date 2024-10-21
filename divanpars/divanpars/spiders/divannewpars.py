468fd7bab829b75a21eed39cff1af0963e6b1770
Alexandr-sys2024 <time2016slot@gmail.com>
2024-10-21 13:45:34 +0300

    add new code in divannewpars

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -4,7 +4,14 @@ import scrapy
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
     allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://www.divan.ru/category/svet?in_stock=1&types%5B%5D=84"]
+    start_urls = ["https://www.divan.ru/category/svet?types%5B%5D=136"]
 
     def parse(self, response):
-        pass
+        luminaires = response.css('div._Ud0k')
+        for luminaire in luminaires:
+            yield{
+                'name' : luminaire.css('div.lsooF span::text').get(),
+                'price': luminaire.css('div.pY3d2 span::text').get(),
+                'url': luminaire.css('a').attrib['href']
+            }
+
