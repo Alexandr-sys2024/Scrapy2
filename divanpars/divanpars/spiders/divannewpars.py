17b5f88ed97486415d0cae5eff443457ba36fe49
Alexandr-sys2024 <time2016slot@gmail.com>
2024-10-23 22:07:59 +0300

    add new code

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -1,17 +1,25 @@
 import scrapy
-import csv
 
 
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
-    allowed_domains = ["https://divan.ru"]
+    allowed_domains = ["divan.ru"]
     start_urls = ["https://www.divan.ru/category/svet?types%5B%5D=136"]
+
+    custom_settings = {
+        'FEEDS': {
+            'luminaires.csv': {
+                'format': 'csv',
+                'fields': ['name', 'price', 'url'],
+            }
+        }
+    }
 
     def parse(self, response):
         luminaires = response.css('div._Ud0k')
         for luminaire in luminaires:
-            yield{
-                'name' : luminaire.css('div.lsooF span::text').get(),
+            yield {
+                'name': luminaire.css('div.lsooF span::text').get(),
                 'price': luminaire.css('div.pY3d2 span::text').get(),
                 'url': luminaire.css('a').attrib['href']
             }
